628b2ab8f7e164ddb01c0f9c88f05b89782fc47d
darco2018 <newsecond@outlook.com>
2019-04-09 08:55:23 +0200

    Add jquery to env in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true
-    },
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true,
+  },
 
-    extends: "airbnb-base",
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module"
-    },
-    rules: {
-        indent: ["error", 2],
-        quotes: ["error", "double"],
-        semi: ["error", "always"]
-    }
+  extends: "airbnb-base",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
