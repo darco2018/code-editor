24d56e275c29a569b00362adbbe40beb724b5d78
darco2018 <newsecond@outlook.com>
2019-04-09 16:22:00 +0200

    Toggle button display

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
+/* eslint-disable no-alert, no-console, no-unused-vars, no-multiple-empty-lines, 
+no-trailing-spaces, max-len, padded-blocks */
 
+/* eslint func-names: ["error", "as-needed"] */
+
+$(document).ready(() => {
+  const $buttons = $(".btn-primary");
+
+  // self-executing function/expression
+  const feature = (function main() {
+    const showHidePanel = function () {
+      const id = $(this).attr("id");
+      let panelId = "#container-";
+
+      switch (id) {
+      case "btn-html":
+        panelId += "html";
+        break;
+      case "btn-css":
+        panelId += "css";
+        break;
+      case "btn-js":
+        panelId += "js";
+        break;
+      case "btn-output":
+        panelId += "result";
+        break;
+      default:
+        break;
+      }
+
+      $(panelId).toggleClass("hidden");
+    };
+
+    $buttons.each(function name() {
+      $(this).click(showHidePanel);
+    });
+  }()); // end main()
+});
